3831e7b1375e8b94da7d7c15a4dd7b6ce89ab19a
Nurullo <86796772+NurulloSalaydinov@users.noreply.github.com>
2022-08-20 11:22:25 +0300

    updated

--- js/app.js
+++ js/app.js
@@ -33,3 +33,17 @@ const NavbarAnimation = () => {
 };
 
 NavbarAnimation();
+
+
+
+{/* <div class="my-5 flex justify-around items-center">
+              <div class="cursor-pointer bg-blue-500 p-2 rounded">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-chevron-up" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708l6-6z"/> </svg>
+              </div>
+              <div>
+                <h4 class="text-lg font-bold text-center">24</h4>
+              </div>
+              <div class="cursor-pointer bg-red-500 p-2 rounded">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-chevron-down" viewBox="0 0 16 16"> <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/> </svg>
+              </div>
+            </div> */}
